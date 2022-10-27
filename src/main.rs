a8f777178c36262f8eedce3f9491b5455bdd5ea4
Manan Karnik <manankarnik03@gmail.com>
2022-10-27 17:30:12 +0530

    feat: read and deserialize config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use directories::ProjectDirs;
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    name: String,
+    color: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(proj_dirs) = ProjectDirs::from("", "", "theme") {
+        let config_dir = proj_dirs.config_dir();
+        let config_file = fs::read_to_string(config_dir.join("config.toml"));
+        let config: Config = match config_file {
+            Ok(file) => toml::from_str(&file).unwrap(),
+            Err(_) => Config {
+                name: "Theme".to_string(),
+                color: "Red".to_string(),
+            },
+        };
+        dbg!(config);
+    }
 }
