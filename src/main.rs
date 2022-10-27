f874ffe4087dc0fc7453048206dc470286dc87fb
Manan Karnik <manankarnik03@gmail.com>
2022-10-27 22:42:15 +0530

    feat: add set subcommand to generate theme

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use clap::Parser;
+
 mod config;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Command {
+    /// Set theme
+    Set { theme: String },
+}
+
 fn main() {
-    let theme: config::Theme = config::get_theme("Monokai Dark");
-    dbg!(theme);
+    let args: Args = Args::parse();
+    match &args.command {
+        Command::Set { theme } => {
+            let theme: config::Theme = config::get_theme(theme);
+            dbg!(theme);
+        }
+    }
 }
